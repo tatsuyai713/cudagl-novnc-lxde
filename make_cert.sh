078bd6088bdc4012ea6fb38017369cb4c4a1ca55
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2023-01-01 04:30:01 +0900

    fix domain name

--- make_cert.sh
+++ make_cert.sh
@@ -3,6 +3,6 @@
 rm -rf ssl
 mkdir ssl
 openssl genrsa 2048 > ssl/nginx.key
-openssl req -new -key ssl/nginx.key -subj "/C=JP/ST=Tokyo/O=Personal Company/CN=my-company.com" > ssl/nginx.csr
+openssl req -new -key ssl/nginx.key -subj "/C=JP/ST=Tokyo/O=Personal Company/CN=localhost" > ssl/nginx.csr
 openssl x509 -days 3650 -req -extfile subjectnames.txt -signkey ssl/nginx.key < ssl/nginx.csr > ssl/nginx.crt
 
